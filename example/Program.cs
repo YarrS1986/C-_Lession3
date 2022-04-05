088481465a2adad989c0180dacd58461e40e2cc7
Yaroslav.S <sysoev.yar@yandex.ru>
2022-04-05 21:46:53 +0500

    закончили урок3. Поменяли текст буквы с К на к. Поработали с массивом

--- example/Program.cs
+++ example/Program.cs
@@ -1,2 +1,91 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Методы.
+
+//Вид 1. ничего не принимает и не возвращает.
+void Method1()
+{
+    Console.WriteLine("Автор...");
+}
+//Method1();
+
+
+//Вид 2. принимает, но не возвращает.
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+//Method2("Текст сообщения");
+//или
+//Method2(msg: "Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method21("Текст сообщения", 4);
+//или
+//Method21(count: 2, msg: "Новый текст");
+
+
+//Вид 3. ничего не принимает, возвращает.
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+//Console.WriteLine(year);
+
+
+
+//Вид 4. принимает и возвращает.
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "Zz");
+// Console.WriteLine(res);
+
+
+
+//цикл For.
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "Zz");
+//Console.WriteLine(res);
+
+
+//цикл в цикле.
+
+Console.Clear();
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
---
+++ example2_arrangeArray/Program.cs
@@ -0,0 +1,38 @@
+﻿// Сортировка массива. от минимального к максимальному
+
+Console.Clear();
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+//несколько вариантов.
+//вариант 1.
+void PrintArray(int[] array) // этот метод задает массив
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array) // упорядочивает массив
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temprorary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temprorary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+Console.WriteLine();
+PrintArray(arr);
